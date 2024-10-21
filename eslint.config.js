7a26a510bbe20e567b9caa14024f8bbaf7ebe205
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2024-10-21 22:31:45 +0300

    add ESLint, Prettier, Husky

---
+++ eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettier from 'eslint-plugin-prettier'; // Імпорт плагіна Prettier
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended
+    ],
+    files: ['**/*.{js,ts}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+    },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.app.json', 'tsconfig.node.json'],
+       },
+    },
+    plugins: {
+      prettier, // Установка плагіна Prettier
+    },
+    rules: {
+      'prettier/prettier': 'error', // Визначення правил які ми встановили для Prettier
+      'no-console': ['error', { allow: ['error', 'warn'] }], // Заборона всіх console крім попереджень/помилок
+      'prefer-const': 'warn', // Попередження при використанні let
+      // Тут ви можете додати будь-які інші правила для JavaScript
+    },
+  }
+);
