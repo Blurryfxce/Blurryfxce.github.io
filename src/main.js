51dab4d9145162209eed95f554643d8aa2967ca3
Say my name <91437948+Blurryfxce@users.noreply.github.com>
2024-10-30 17:35:27 +0200

    Delete src directory

--- src/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-// Налаштовуємо модальне вікно
-(0, setupModal)();
-// Налаштовуємо анімацію при скролі
-(0, setupHeaderScrollAnimation)();
-// Завантажуємо дані через fetch
-(0, fetchPosts)();
--- src/modules/animations.ts
+++
@@ -1,9 +0,0 @@
-export const header: HTMLElement | null = document.getElementById("header");
-
-export function setupHeaderScrollAnimation(): void {
-    if (!header) return;
-
-    window.addEventListener("scroll", () => {
-        header.style.backgroundColor = window.scrollY > 50 ? "#555" : "#333";
-    });
-}
